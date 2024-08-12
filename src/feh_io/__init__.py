92f43f2f6b94a796e3c7a2b39f702d5504fc3b94
Jameson Carter <jamcarter@urban.org>
2024-08-12 11:05:57 -0400

    update __init__ for desired namespace behavior

--- src/feh_io/__init__.py
+++ src/feh_io/__init__.py
@@ -3,3 +3,5 @@ Intialization file necessary for the package to be recognized as a module.
 
 This can be empty.
 """
+from .read_feh import read_feh_data_file
+from .save_feh import save_feh_parquet
