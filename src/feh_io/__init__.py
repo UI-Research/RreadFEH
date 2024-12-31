c20ca94221622b660bdd5122e6daa7c0ac8262b6
Damir Cosic <dcosic@urban.org>
2024-12-31 16:28:06 -0500

    Exports read_header_file.

--- src/feh_io/__init__.py
+++ src/feh_io/__init__.py
@@ -3,5 +3,5 @@ Intialization file necessary for the package to be recognized as a module.
 
 This can be empty.
 """
-from .read_feh import read_feh_data_file
+from .read_feh import read_feh_data_file, read_header_file
 from .save_feh import save_feh_parquet
