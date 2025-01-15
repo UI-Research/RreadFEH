0c2eb58d2f37deb516124aa85a255fa41efcc785
Damir Cosic <dcosic@urban.org>
2025-01-15 11:59:11 -0500

    Fixes a problem with exporting FehReader

--- src/feh_io/__init__.py
+++ src/feh_io/__init__.py
@@ -3,5 +3,6 @@ Intialization file necessary for the package to be recognized as a module.
 
 This can be empty.
 """
-from .read_feh import read_feh_data_file, read_header_file, feh_wide_to_long, FehReader
+from .read_feh import read_feh_data_file, read_header_file, feh_wide_to_long
+from feh_reader import FehReader
 from .save_feh import save_feh_parquet
